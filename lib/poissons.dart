3d1c2aa7ab7355abcc7313a2f20ddd59b663bc12
GaPiko <57481107+GaPiko-dev@users.noreply.github.com>
2021-06-30 12:03:08 +0200

    On s'en fout

--- lib/poissons.dart
+++ lib/poissons.dart
@@ -8,16 +8,27 @@ class PoissonsWidget extends StatefulWidget {
 }
 
 class _PoissonsWidgetState extends State<PoissonsWidget> {
+  void ButtonClick()
+  {
+    print("mdr");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          'kljhgfd',
-        ), Text(
-          'kljhgfd',
-        ),
+        Row(children: [
+          Image.network('https://image.flaticon.com/icons/png/512/743/743418.png',
+            scale:10
+          ),
+          Text("Ammonium en grande concentration")
+        ]),
+        Image.network(
+            'https://static.vecteezy.com/ti/vecteur-libre/t2/2534491-poisson-rouge-dessine-a-la-main-gratuit-vectoriel.jpg',
+            scale: 0.8),
+        FlatButton(onPressed: ButtonClick, child: Text("Clique pour changer de couleur"))
       ],
+      
     );
   }
 }
